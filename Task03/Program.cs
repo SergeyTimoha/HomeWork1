e0894741d40479ba90338db725a394ba4d804f0c
Sergey <serega79ser@mail.ru>
2022-04-12 22:38:09 +0500

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+
+WriteLine("Введите первое число");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число");
+int b = Convert.ToInt32(ReadLine());
+WriteLine("Введите третье число");
+int с = Convert.ToInt32(ReadLine());
+int max = a;
+if (max < b)
+{
+    max = b;
+    WriteLine(max);
+}
+if (max < c);
+{
+    WriteLine(max);
+    max = c;
+}
+WriteLine(max);
