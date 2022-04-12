c98ccf4aa025992f9b0d571153a7ad55d114eba8
Sergey <serega79ser@mail.ru>
2022-04-12 20:56:15 +0500

    Task02 Найти максимальное из двух чисел

---
+++ Task02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+
+WriteLine("Введите первое число");
+int a = int.Parse(ReadLine());
+WriteLine("Введите второе число");
+int b = int.Parse(ReadLine());
+int  max, min;
+if (a > b)
+{
+   max = a;
+   min = b;
+   WriteLine("Первое число max = " + a  );
+   WriteLine("Второе число min = " + b  );
+
+}
+else
+{
+    max = b;
+    min = a;
+    WriteLine("Второе число max = " + b);
+    WriteLine("Первое число min = " + a  );
+
+}
+
