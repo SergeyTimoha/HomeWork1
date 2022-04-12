3ce0fa96da7c9dfac95bb40bc272a9183dbe9c7e
Sergey <serega79ser@mail.ru>
2022-04-12 18:28:13 +0500

    Task01 Найти чётное число

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите число");
+int number = int.Parse(ReadLine());
+if (number % 2 == 0)
+{
+    WriteLine("Чётное число");
+}
+else
+{
+    WriteLine("Нечётное число");
+}
