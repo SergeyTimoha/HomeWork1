e6dc290c9d79f505dc74d23c836e306a4043dbeb
Sergey <serega79ser@mail.ru>
2022-04-13 22:03:53 +0500

    Task05 Добавили 2 задачу

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+
+WriteLine("Введите первое число");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число");
+int b = Convert.ToInt32(ReadLine());
+WriteLine("Введите третье число");
+int c = Convert.ToInt32(ReadLine());
+int max = a;
+if (b > max) 
+{
+    max = b;
+}
+if (c > max) 
+{
+    max = c;
+}
+
+WriteLine("max = " + max);
