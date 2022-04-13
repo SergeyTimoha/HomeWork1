8b961ef52171f3a812b164a8391c416f4f18c97d
Sergey <serega79ser@mail.ru>
2022-04-13 20:42:33 +0500

    Task

---
+++ Task04/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using static System.Console;
+
+WriteLine("Введите число");
+int N = Convert.ToInt32(ReadLine());
+int b = 0;
+
+while(b < N)
+{
+    if(N % 2 == 0)
+    {
+         
+         WriteLine("Чётные числа = " );
+         
+        
+         b++;
+          
+    } 
+    
+    
+      
+ 
+
+
+
+}
+
+
+
