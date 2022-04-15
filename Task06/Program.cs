9f1834a8d6a64542c5116a7eb17e0c01a423ba24
Sergey <serega79ser@mail.ru>
2022-04-15 09:48:03 +0600

    Task06 Задача № 4

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+
+Write("Введите первое число: ");
+int N = Convert.ToInt32(ReadLine());
+
+int  C = 2;
+while (C < N)
+{
+    WriteLine(C);
+    C = C + 2;
+}
