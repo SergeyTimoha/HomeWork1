d0b58574e2f790aaf04af62fc6514eb3f06be181
Sergey <serega79ser@mail.ru>
2022-04-16 23:14:36 +0600

    Task22 Задача 13,Третья цифра заданного числа

---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+Console.Clear(); 
+Console.Write("Введите любое число: ");
+int num = new Random().Next(0,1000);
+string numS=num.ToString();
+Console.WriteLine(num);
+if(num > 99)
+{
+    Console.WriteLine("Третья цифра заданного числа = " + numS[2]);
+}
+else
+    Console.WriteLine("Третьего числа нет!");
+
