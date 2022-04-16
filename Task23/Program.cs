6f93345f4bfdb2093f2c34755581b2c3e29e222c
Sergey <serega79ser@mail.ru>
2022-04-16 23:33:55 +0600

    Task23 Задача 15: является ли этот день выходным

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿
+/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающу
+ю день недели, и проверяет, является ли этот день выходным.*/
+Console.Clear(); 
+Console.Write("Введите число: ");
+int num = new Random().Next(1,8);
+Console.WriteLine(num);
+if(num > 5)
+{
+    Console.WriteLine("Да, выходной = " + num);
+}
+else
+    Console.WriteLine("Нет, идём на работу");
