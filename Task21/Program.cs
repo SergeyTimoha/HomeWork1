a42c227f0cf033877773a7bd1ac712f0d40ae6ff
Sergey <serega79ser@mail.ru>
2022-04-16 22:46:16 +0600

    Task21 на вход трёхзначное число и на выходе показывает вторую цифру этого числ

---
+++ Task21/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+
+Console.Clear(); 
+Console.Write("Введите трёхзначное число: ");
+int num = new Random().Next(100,1000);
+string numS=num.ToString();
+int second = Convert.ToInt32(numS[1].ToString());
+
+Console.WriteLine(num);
+
+Console.WriteLine("Вторая цифра числа = " + second);
+
+
