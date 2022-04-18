44d079939c14caa8c726593047d12196f4c80d87
Sergey <serega79ser@mail.ru>
2022-04-18 12:14:50 +0600

    Task04 чётное не чётное

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,27 +4,21 @@ using static System.Console;
 
 WriteLine("Введите число");
 int N = Convert.ToInt32(ReadLine());
-int b = 0;
 
-while(b < N)
-{
-    if(N % 2 == 0)
+if(N % 2 == 0)
     {
-         
-         WriteLine("Чётные числа = " );
-         
-        
-         b++;
-          
+        WriteLine("Чётные числа = " + N);
     } 
-    
-    
+else    
+    {
+        WriteLine("Нечётные числа = " + N);
+    }
       
  
 
 
 
-}
+
 
 
 
