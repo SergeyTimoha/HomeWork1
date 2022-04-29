32cb468a26dd3e3ada6cfa28272ae42434a9dede
Sergey <serega79ser@mail.ru>
2022-04-29 14:52:07 +0600

    Задача 34

---
+++ Task51_Home/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет количество 
+чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+using System;
+using System.Linq;
+
+Console.Clear();
+int[] numbers = FillArray(10, 100, 999);
+Console.WriteLine(string.Join(" ", numbers));
+Console.WriteLine($"Число чётных элементов в массиве: {EvenNumbers(numbers)}.");
+
+int[] FillArray(int size, int mixValue, int maxValue)
+{
+    int[] getArray = new int[size].Select(x => new Random().Next(mixValue, maxValue + 1)).ToArray();
+    return getArray;
+}
+
+int EvenNumbers(int[] numArray)
+{
+    int counter = numArray.Where(x => x % 2 == 0).Count();
+    return counter;
+}
